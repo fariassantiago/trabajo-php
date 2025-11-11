94cacb1a385d1c0c62c6f6989c66790a760f714a
Marto <mf2199838@gmail.com>
2025-11-11 00:27:24 -0300

    BD conectada + alta.php listo

--- conexion.php
+++ conexion.php
@@ -1,11 +1,9 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "iac");
+$conexion = mysqli_connect("localhost", "root", "", "iac", 3307);
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 } else {
     echo "Conexión exitosa a la base de datos IAC.";
 }
-
-mysqli_close($conexion);
 ?>
---
+++ test_post.php
@@ -0,0 +1,12 @@
+<?php
+// Muestra errores (temporal, para debug)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Método recibido: " . $_SERVER['REQUEST_METHOD'] . "<br>";
+echo "Contenido de \$_POST:<br>";
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+?>
